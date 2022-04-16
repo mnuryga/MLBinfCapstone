61a3196d378e7bed8fbb74697dd45995021002b1
Matthew Uryga <mnuryga@gmail.com>
2022-04-16 19:54:06 -0400

    implemented batching and processing of msa_representation and pariwise_representation

--- Evo_Dataset.py
+++ Evo_Dataset.py
@@ -1,21 +1,49 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import IterableDataset
+from torch.utils.data import DataLoader
 
 import sidechainnet as scn
 import numpy as np
 from tqdm import tqdm
 import sys
-from einops import rearrange
+from einops import rearrange, repeat
+
+from Models import PSSM_Projector
+from Models import Input_Feature_Projector
+from Models import Residue_Index_Projector
+
+USE_DEBUG_DATA = True
+progress_bar = False
 
 class Evo_Dataset(IterableDataset):
-	def __init__(self, key, stride):
+	def __init__(self, key, stride, N_res = 256, N_clust = 16, c_m = 256, c_2 = 128, randomize_start = True):
 		super().__init__()
+		self.randomize_start = randomize_start
 		self.key = key
 		self.stride = stride
-		self.data = scn.load(casp_version = 7, with_pytorch="dataloaders", seq_as_onehot=True,
-													aggregate_model_input=False, batch_size=16, num_workers = 0)
+		self.N_res = N_res
+		self.N_clust = N_clust
+		self.c_m = c_m
+		self.c_2 = c_2
+		self.pad2d = nn.ZeroPad2d((0, N_res, 0, N_res))
+		if USE_DEBUG_DATA:
+			self.data = scn.load('debug', with_pytorch="dataloaders", seq_as_onehot=True, aggregate_model_input=False, batch_size=16, num_workers = 0)
+		else:
+			self.data = scn.load(casp_version = 7, with_pytorch="dataloaders", seq_as_onehot=True, aggregate_model_input=False, batch_size=16, num_workers = 0)
 
-	@classmethod
+		# CHECK IF THIS WILL BACKPROP PROPERLY
+		self.pssm_projector = PSSM_Projector(N_clust, c_m)
+		self.input_feature_projector = Input_Feature_Projector(c_2)
+		self.residue_index_projector = Residue_Index_Projector(c_2)
+
+	def pad1d(x):
+		out = torch.zeros(len(x)+self.N_res)
+		out[:len(x)] = x
+		return out
+
+	@staticmethod
 	def get_seq_features(batch):
 		'''
 		Take a batch of sequence info and return the sequence (one-hot),
@@ -52,11 +80,72 @@ class Evo_Dataset(IterableDataset):
 		return seqs, evos, angs, masks, dmats, dmat_masks
 
 	def __iter__(self):
+		# loop over sets of sequences with same length
 		for batch_idx, batch in enumerate(tqdm(self.data[self.key], disable = not progress_bar)):
-			seqs, evos, angs, masks, dmats, dmat_masks = get_seq_features(batch)
+			seqs, evos, angs, masks, dmats, dmat_masks = self.get_seq_features(batch)
 
 			# get dimensions for feature matrix
+			B = seqs.shape[0]
 			L = seqs.shape[1]
 
 			# discreteize dmat in 64 bins
-			dmat = torch.floor(torch.clamp(dmat, 2, 21.6875).sub(2).mul(3.2))
+			# dmats = torch.floor(torch.clamp(dmats, 2, 21.6875).sub(2).mul(3.2))
+
+			# pad data
+			seqs = F.pad(seqs, (0, 1, 0, self.N_res), 'constant', 0)
+			evos = F.pad(evos, (0, 0, 0, self.N_res), 'constant', 0)
+			# masks = F.pad(masks, (0, self.N_res), 'constant', 0)
+			# dmats = F.pad(dmats, (0, self.N_res, 0, self.N_res), 'constant', 0)
+			# dmat_masks = F.pad(dmat_masks, (0, self.N_res, 0, self.N_res), 'constant', 0)
+
+			# get PSSM data projections
+			msa_reps = self.pssm_projector(evos)
+
+			# get residue index and target feat projections
+			l1, l2 = self.input_feature_projector(seqs.float())
+
+			# calculate outer sum
+			l1 = repeat(l1, 'b i c -> b rep i c', rep = L + self.N_res)
+			l2 = repeat(l2, 'b i c -> b rep i c', rep = L + self.N_res)
+			l2 = rearrange(l2, 'b i j c -> b j i c')
+			outer_sum = l1.add(l2)
+
+			# calculate relative positional encodings
+			all_res = torch.arange(L+self.N_res)
+			di = repeat(all_res, 'i -> rep i', rep = L + self.N_res)
+			dj = repeat(-all_res, 'j -> rep j', rep = L + self.N_res)
+			dj = rearrange(dj, 'i j -> j i')
+
+			# clamp differences and encode as onehot
+			d = torch.clamp(di.add(dj), -32, 32).add(32)
+			d = F.one_hot(d)
+
+			# pass through linear layer
+			relpos_encoding = self.residue_index_projector(d.float())
+
+			# create pairwise representation
+			pairwise_reps = outer_sum.add(relpos_encoding)
+
+			# for each sequence of length L
+			for pairwise_rep, msa_rep in zip(pairwise_reps, msa_reps):
+				# get crops of length N_res
+				# generate starting position for window
+				start_i = 0 if L < 64 or not self.randomize_start else np.random.randint(0, 64)
+				for i in range(start_i, L, self.stride):
+					yield pairwise_rep[i:i+self.N_res, i:i+self.N_res], msa_rep[:, i:i+self.N_res]
+
+
+# main function for testing
+def main():
+	ds = Evo_Dataset('test', 128)
+	dl = DataLoader(dataset = ds, batch_size = 5)
+	for i, (pr, mr) in enumerate(dl):
+		print()
+		print(i)
+		print(f'{pr.shape = }')
+		print(f'{mr.shape = }')
+		if i > 20:
+			sys.exit(0)
+
+if __name__ == '__main__':
+	main()
--- Models.py
+++ Models.py
@@ -13,3 +13,38 @@ class Evoformer_Model(nn.Module):
 	
 	def forward(self, x):
 		pass
+
+class PSSM_Projector(nn.Module):
+	def __init__(self, num_layers, c_m):
+		super().__init__()
+		layers = [nn.Linear(21, c_m) for i in range(num_layers)]
+		self.layers = nn.ModuleList(layers)
+		self.c_m = c_m
+		self.num_layers = num_layers
+	
+	def forward(self, x):
+		out = torch.zeros((x.shape[0], self.num_layers, x.shape[1], self.c_m))
+		for i in range(x.shape[0]):
+			for j, l in enumerate(self.layers):
+				out[i,j] = l(x[i])
+		return out
+
+class Input_Feature_Projector(nn.Module):
+	def __init__(self, c_2):
+		super().__init__()
+		self.c_2 = c_2
+		self.l1 = nn.Linear(21, c_2)
+		self.l2 = nn.Linear(21, c_2)
+	
+	def forward(self, x):
+		x1 = self.l1(x)
+		x2 = self.l2(x)
+		return x1, x2
+
+class Residue_Index_Projector(nn.Module):
+	def __init__(self, c_2):
+		super().__init__()
+		self.l = nn.Linear(65, c_2)
+	
+	def forward(self, x):
+		return self.l(x)
